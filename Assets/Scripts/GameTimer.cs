c4ed10a00747121afa37d4441da99f2bff2a2f02
Alberto <albertodelcru@gmail.com>
2017-07-28 21:14:34 +0200

    Game timer and main menu script

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    private Text timerText;
+    private float timer;
+
+    void Awake()
+    {
+        timerText = GameObject.FindGameObjectWithTag("GameTimer").GetComponent<Text>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+        timerText.text = minutes + " : " + seconds;
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    private GameObject MainPanel, RankingPanel;
+
+    void Awake()
+    {
+        MainPanel = GameObject.Find("MainPanel");
+        RankingPanel = GameObject.Find("RankingPanel");
+        RankingPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenRanking()
+    {
+        MainPanel.SetActive(false);
+        RankingPanel.SetActive(true);
+    }
+
+    public void CloseRanking()
+    {
+        RankingPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+}
