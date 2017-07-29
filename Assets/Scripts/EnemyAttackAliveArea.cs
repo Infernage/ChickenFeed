18440c4a5b84a598d93a193e8f5edd1f7a0e788a
Carlos Fernández Llamas <sirikon2@gmail.com>
2017-07-29 16:55:16 +0200

    EnemyAttack

---
+++ Assets/Scripts/EnemyAttackAliveArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackAliveArea : MonoBehaviour {
+
+    private EnemyAttack _enemyAttack;
+
+    public void SetEnemyAttack(EnemyAttack enemyAttack)
+    {
+        _enemyAttack = enemyAttack;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("TriggerExit");
+        var enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            _enemyAttack.Stop();
+        }
+    }
+}
