58ad2f97408a759e4f45607fe842be19440b1328
Escobeitor <borjascripter@gmail.com>
2017-07-29 18:39:12 +0200

    Particles and images

--- Assets/Scripts/PlaceFeedWithMouse.cs
+++ Assets/Scripts/PlaceFeedWithMouse.cs
@@ -8,22 +8,34 @@ public class PlaceFeedWithMouse : MonoBehaviour {
 
     public PiensoManager feedManager;
 
+    Texture2D HandClosed;
+    Texture2D HandOpen;
+
     // Use this for initialization
     void Start () {
 
         feedManager = gameObject.AddComponent<PiensoManager>();
+        
+        HandClosed = Resources.Load("Sprites/Hand_Closed") as Texture2D;
+        HandOpen = Resources.Load("Sprites/Hand_Open") as Texture2D;
 
-
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
+
+        if (!Input.GetMouseButton(0))
+        {
+            Cursor.SetCursor(HandClosed, Vector2.zero, CursorMode.Auto);
+            return;
+        }
+        Cursor.SetCursor(HandOpen, Vector2.zero, CursorMode.Auto);
+
         if (!Input.GetMouseButtonDown(0))
         {
             return;
         }
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (!Physics.Raycast(ray, out hit))
@@ -35,6 +47,7 @@ public class PlaceFeedWithMouse : MonoBehaviour {
 
 
         feedManager.AddPienso(hit.point + hit.normal);
+
         
 
     }
