a357ecba5c86dd98a5a7096f89660990fc760d02
Escobeitor <borjascripter@gmail.com>
2017-07-29 15:55:21 +0200

    Feed shrinker

--- Assets/Scripts/Pienso.cs
+++ Assets/Scripts/Pienso.cs
@@ -6,15 +6,29 @@ public class Pienso : MonoBehaviour {
 
     public float FeedLifeTime;
     public float RemainingTime;
+    public float TimePercToDissapear; // in decimal f i.e. 0.2f = 20%
     public Vector2 Location=new Vector2(0,0);
+
+    float TimeShrinkStart;
+    float TimeShrinkEnd;
+    float ShrinkLength;
+
+    Vector3 FinalScale;
 
     public SpriteRenderer FeedSpriteRender;   
 
 	// Use this for initialization
 	public void Start () {
-        FeedLifeTime = 5.0f;
+        //FeedLifeTime = 5.0f;
+        //TimePercToDissapear = 0.5f;
         RemainingTime = FeedLifeTime;
         transform.position = Location;
+
+        TimeShrinkStart = FeedLifeTime * TimePercToDissapear;
+
+        ShrinkLength = TimeShrinkStart - TimeShrinkEnd;
+
+        FinalScale = new Vector3(0, 0, 0);
 
         Debug.Log("Feed start event");
     }
@@ -24,6 +38,13 @@ public class Pienso : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         RemainingTime = RemainingTime - Time.deltaTime;
+
+        if(RemainingTime <= FeedLifeTime * TimePercToDissapear)
+        {
+            float ShrinkCovered = (TimeShrinkStart - RemainingTime);
+            float ShrinkFraction = ShrinkCovered / ShrinkLength;
+            transform.localScale = Vector3.Lerp(new Vector3(1, 1, 1), FinalScale, ShrinkFraction);
+        }
 	}
 
     public void KillFeed()
